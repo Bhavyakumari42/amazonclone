4662ba26e7eeba6a8b204830c5482c01c5a04974
bhavya <bhavyakumari42@gmail.com>
2024-01-31 20:23:42 +0530

    third commit

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const signin = document.querySelector(".content.border");
 const hidden = document.querySelector(".main-hidden");
+const menubar = document.querySelector(".menu-bar");
+
 signin.addEventListener('mouseover', ()=>
 {
   hidden.style.display = "grid";
 }
     );
-    signin.addEventListener('mouseout', ()=>
+   hidden.addEventListener('mouseover', ()=>
 {
-  hidden.style.display = "none";
+  hidden.style.display = "grid";
 }
     );
+    signin.addEventListener('mouseout', () =>
+    {
+      hidden.style.display = "none";
+    })
+   hidden.addEventListener('mouseout', () =>
+    {
+      hidden.style.display = "none";
+    })
